ffee654328d37ee71ddd96a2ddb78a8ce28e67a9
Sabit <sabit990928@gmail.com>
2019-12-21 11:52:04 +0600

    feat: introduce IStudentRepo and StudentRepo

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using stable.Data;
+using stable.Services;
 
 namespace stable {
     public class Startup {
@@ -10,9 +11,13 @@ namespace stable {
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices (IServiceCollection services) {
             services.AddDbContext<ProjectContext> (options => {
-                options.UseSqlite ("Filename=university.db");
+                // options.UseSqlite ("Filename=university.db");
+                options.UseSqlite ("Filename=university2.db");
             });
             services.AddMvc ();
+            services.AddScoped<DepartmentService> ();
+            services.AddScoped<StudentService> ();
+            services.AddScoped<IStudentRepo, StudentRepo> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
