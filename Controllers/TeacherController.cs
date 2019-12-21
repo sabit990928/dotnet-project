76b3cbaaca7a980a9e80946fb4e99cf4e513dec9
Sabit <sabit990928@gmail.com>
2019-12-21 11:51:20 +0600

    style: show group and dep name in list

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -7,48 +7,40 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using stable.Models.Teachers;
 
-namespace stable.Controllers
-{
-    public class TeacherController : Controller
-    {
+namespace stable.Controllers {
+    public class TeacherController : Controller {
         private readonly ProjectContext _context;
 
-        public TeacherController(ProjectContext context)
-        {
+        public TeacherController (ProjectContext context) {
             _context = context;
         }
 
         // GET: Teacher
-        public async Task<IActionResult> Index()
-        {
-            var projectContext = _context.Teachers.Include(t => t.Department);
-            return View(await projectContext.ToListAsync());
+        public async Task<IActionResult> Index () {
+            var projectContext = _context.Teachers.Include (t => t.Department);
+            return View (await projectContext.ToListAsync ());
         }
 
         // GET: Teacher/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
+        public async Task<IActionResult> Details (int? id) {
+            if (id == null) {
+                return NotFound ();
             }
 
             var teacher = await _context.Teachers
-                .Include(t => t.Department)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (teacher == null)
-            {
-                return NotFound();
+                .Include (t => t.Department)
+                .FirstOrDefaultAsync (m => m.Id == id);
+            if (teacher == null) {
+                return NotFound ();
             }
 
-            return View(teacher);
+            return View (teacher);
         }
 
         // GET: Teacher/Create
-        public IActionResult Create()
-        {
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Id");
-            return View();
+        public IActionResult Create () {
+            ViewData["DepartmentId"] = new SelectList (_context.Departments, "Id", "Name");
+            return View ();
         }
 
         // POST: Teacher/Create
@@ -56,33 +48,29 @@ namespace stable.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,DepartmentId")] Teacher teacher)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(teacher);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+        public async Task<IActionResult> Create ([Bind ("Id,Name,DepartmentId")] Teacher teacher) {
+            if (ModelState.IsValid) {
+                _context.Add (teacher);
+                await _context.SaveChangesAsync ();
+                return RedirectToAction (nameof (Index));
             }
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Id", teacher.DepartmentId);
-            return View(teacher);
+            // Console.WriteLine ('adf');
+            ViewData["DepartmentId"] = new SelectList (_context.Departments, "Id", "Name", teacher.DepartmentId);
+            return View (teacher);
         }
 
         // GET: Teacher/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
+        public async Task<IActionResult> Edit (int? id) {
+            if (id == null) {
+                return NotFound ();
             }
 
-            var teacher = await _context.Teachers.FindAsync(id);
-            if (teacher == null)
-            {
-                return NotFound();
+            var teacher = await _context.Teachers.FindAsync (id);
+            if (teacher == null) {
+                return NotFound ();
             }
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Id", teacher.DepartmentId);
-            return View(teacher);
+            ViewData["DepartmentId"] = new SelectList (_context.Departments, "Id", "Name", teacher.DepartmentId);
+            return View (teacher);
         }
 
         // POST: Teacher/Edit/5
@@ -90,70 +78,56 @@ namespace stable.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DepartmentId")] Teacher teacher)
-        {
-            if (id != teacher.Id)
-            {
-                return NotFound();
+        public async Task<IActionResult> Edit (int id, [Bind ("Id,Name,DepartmentId")] Teacher teacher) {
+            if (id != teacher.Id) {
+                return NotFound ();
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(teacher);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TeacherExists(teacher.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
+            if (ModelState.IsValid) {
+                try {
+                    _context.Update (teacher);
+                    await _context.SaveChangesAsync ();
+                } catch (DbUpdateConcurrencyException) {
+                    if (!TeacherExists (teacher.Id)) {
+                        return NotFound ();
+                    } else {
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction (nameof (Index));
             }
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Id", teacher.DepartmentId);
-            return View(teacher);
+            ViewData["DepartmentId"] = new SelectList (_context.Departments, "Id", "Id", teacher.DepartmentId);
+            return View (teacher);
         }
 
         // GET: Teacher/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
+        public async Task<IActionResult> Delete (int? id) {
+            if (id == null) {
+                return NotFound ();
             }
 
             var teacher = await _context.Teachers
-                .Include(t => t.Department)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (teacher == null)
-            {
-                return NotFound();
+                .Include (t => t.Department)
+                .FirstOrDefaultAsync (m => m.Id == id);
+            if (teacher == null) {
+                return NotFound ();
             }
 
-            return View(teacher);
+            return View (teacher);
         }
 
         // POST: Teacher/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName ("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var teacher = await _context.Teachers.FindAsync(id);
-            _context.Teachers.Remove(teacher);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+        public async Task<IActionResult> DeleteConfirmed (int id) {
+            var teacher = await _context.Teachers.FindAsync (id);
+            _context.Teachers.Remove (teacher);
+            await _context.SaveChangesAsync ();
+            return RedirectToAction (nameof (Index));
         }
 
-        private bool TeacherExists(int id)
-        {
-            return _context.Teachers.Any(e => e.Id == id);
+        private bool TeacherExists (int id) {
+            return _context.Teachers.Any (e => e.Id == id);
         }
     }
 }
