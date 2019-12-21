460362d2fff43cf68e339f91eec2ad6b106c1f45
Sabit <sabit990928@gmail.com>
2019-12-21 12:34:06 +0600

    refactor: add tests folder

---
+++ tests/StudentTest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using stable.Data;
+using stable.Models.Students;
+using Xunit;
+
+namespace Tests {
+    public class DeveloperTest {
+
+        List<Student> students = new List<Student> { };
+        Mock<IStudentRepo> studentRepository;
+
+    }
+}
